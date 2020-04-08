8aaac96b3122b3e904e2ad2b5d582d85eb511206
Norma Lopez <norma_l304@yahoo.com>
2020-04-08 09:46:20 -0600

    Add test for attributes

--- test/game_test.rb
+++ test/game_test.rb
@@ -11,4 +11,12 @@ class GameTest < Minitest::Test
     assert_instance_of Game, pick_4
     assert_instance_of Game, mega_millions
   end
+
+  def test_it_has_attributes
+    mega_millions = Game.new('Mega Millions', 5, true)
+
+    assert_equal "Mega Millions", mega_millions.name
+    assert_equal 5, mega_millions.cost
+    assert_equal true, mega_millions.national_drawing?
+  end 
 end
