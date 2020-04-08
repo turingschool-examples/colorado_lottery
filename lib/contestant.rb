56851cb56c5b564c5804febe55963b21dda8dbd3
Kiera Allen <kieraaallen@gmail.com>
2020-04-08 09:32:16 -0600

    Test and add Contestant has attributes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,5 +1,19 @@
 class Contestant
+  attr_reader :first_name,
+              :last_name,
+              :age,
+              :state_of_residence,
+              :spending_money
 
   def initialize(contestant_info)
+    @first_name = contestant_info[:first_name]
+    @last_name = contestant_info[:last_name]
+    @age = contestant_info[:age]
+    @state_of_residence = contestant_info[:state_of_residence]
+    @spending_money = contestant_info[:spending_money]
   end
+
+  def full_name
+  end
+
 end
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -16,4 +16,16 @@ class ContestantTest < Minitest::Test
     assert_instance_of Contestant, @alexander
   end
 
+  def test_it_has_attributes
+    assert_equal "Alexander", @alexander.first_name
+    assert_equal "Aigiades", @alexander.last_name
+    assert_equal 28, @alexander.age
+    assert_equal "CO", @alexander.state_of_residence
+    assert_equal 10, @alexander.spending_money
+  end
+
+  # def test_it_has_full_name
+  #   assert_equal "Alexander Aigiades", @alexander.full_name
+  # end
+
 end
