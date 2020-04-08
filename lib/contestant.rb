810e78667b46048d02489a65d9b8508f22512287
Judith Pillado <judith.pillado@gmail.com>
2020-04-08 10:45:02 -0500

    Add attributes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,2 +1,15 @@
 class Contestant
-end 
+  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
+
+  def initialize(attribute)
+    @first_name         = attribute[:first_name]
+    @last_name          = attribute[:last_name]
+    @age                = attribute[:age]
+    @state_of_residence = attribute[:state_of_residence]
+    @spending_money     = attribute[:spending_money]
+  end
+
+  def full_name
+    "#{first_name} ".concat(last_name)
+  end
+end
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -3,4 +3,26 @@ require 'minitest/pride'
 require './lib/contestant'
 
 class ContestantTest < Minitest::Test
+
+  def setup
+    @alexander = Contestant.new({first_name: 'Alexander',
+                                last_name: 'Aigiades',
+                                age: 28,
+                                state_of_residence: 'CO',
+                                spending_money: 10})
+  end
+
+  def test_it_exists
+    assert_instance_of Contestant, @alexander
+  end
+
+  def test_full_name
+    assert_equal "Alexander Aigiades", @alexander.full_name
+  end
+
+  def test_it_has_attributes
+    assert_equal 28, @alexander.age
+    assert_equal "CO", @alexander.state_of_residence
+    assert_equal 10, @alexander.spending_money
+  end
 end
