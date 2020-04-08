dec900abbc6f57d20eceff3f623cb2d717b225ca
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 09:13:14 -0600

    Initialize with attributes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,1 +1,10 @@
-class Contestant 
+class Contestant
+
+  def initialize(contestant_info)
+    @first_name = contestant_info[:first_name]
+    @last_name = contestant_info[:last_name]
+    @age = contestant_info[:age]
+    @state_of_residence = contestant_info[:state_of_residence]
+    @spending_money = contestant_info[:spending_money]
+  end
+end 
