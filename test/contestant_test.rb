645c8dba65da5ff90321fd16affb9f226c8fb869
Eric Hale <eric_hale@comcast.net>
2020-04-08 09:50:11 -0600

    Add test it exists test

---
+++ test/contestant_test.rb
@@ -0,0 +1,15 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/contestant'
+require 'pry'
+
+class ContestantTest < Minitest::Test
+  def test_it_exists
+    alexander = Contestant.new({first_name: 'Alexander',
+                                      last_name: 'Aigiades',
+                                      age: 28,
+                                      state_of_residence: 'CO',
+                                      spending_money: 10})
+    assert_instance_of Contestant, alexander
+  end
+end
