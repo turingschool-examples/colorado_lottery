c1f538f1d6f1f8008e8db60cb673bd984e3f78de
Michael Gallup <Gallup93@icloud.com>
2020-04-08 09:22:32 -0600

    finished initializtion process

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,9 +1,19 @@
 class Contestant
-  attr_reader :full_name, :age, :state_of_residence, :spending_money
+  attr_reader :full_name, :age, :state_of_residence, :spending_money, :game_interests
   def initialize(info)
     @full_name = info[:first_name] + " " + info[:last_name]
     @age = info[:age]
     @state_of_residence = info[:state_of_residence]
     @spending_money = info[:spending_money]
+    @out_of_state = out_of_state?
+    @game_interests = []
+  end
+
+  def out_of_state?
+    if @state_of_residence == "CO"
+      false
+    else
+      true
+    end
   end
 end
