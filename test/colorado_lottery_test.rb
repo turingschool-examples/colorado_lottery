778d0df9d9e9454824f30269a4bfcbcd3db1c6c7
Sage Lee <sagemcklee@gmail.com>
2020-04-08 09:52:18 -0600

    Add lottery contestant empty attributes test/method

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -11,4 +11,10 @@ class ColoradoLotteryTest < Minitest::Test
     lottery = ColoradoLottery.new
     assert_instance_of ColoradoLottery, lottery
   end
+  def test_it_starts_with_no_contestants_or_winners
+    lottery = ColoradoLottery.new
+    assert_empty lottery.registered_contestants
+    assert_empty lottery.winners
+    assert_empty lottery.current_contestants
+  end
 end
