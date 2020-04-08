08d63f15b2c1d2ca0355ba168d3454234813c1f4
Taylor Keller <takeller17@gmail.com>
2020-04-08 10:03:15 -0600

    Added can_register? method

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -12,4 +12,10 @@ class ColoradoLottery
     return true if contestant.game_interests.include?(game.name) && contestant.age >= 18
     false
   end
+
+  def can_register?(contestant, game)
+    return false if interested_and_18?(contestant, game) == false
+    return false if game.national_drawing? == false && contestant.out_of_state? == true
+    true
+  end
 end
