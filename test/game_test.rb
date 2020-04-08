ba941185dd1adea1e05620e146c4a17c5e3cd202
James Belta <james.belta@gmail.com>
2020-04-08 09:29:42 -0600

    Startted game test

---
+++ test/game_test.rb
@@ -0,0 +1,12 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/contestant'
+require './lib/game/'
+
+class GameTest < Minitest::Test
+  def test_it_exist
+    pick_4 = Game.new('Pick 4', 2)
+
+    assert_instance_of Game, pick_4
+  end
+end
