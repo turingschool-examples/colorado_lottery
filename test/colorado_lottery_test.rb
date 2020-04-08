92607961f8510383afa6cf1afffae86ed0f123b1
Derek Borski <derek.borski@gmail.com>
2020-04-08 09:48:44 -0600

    Creates test for interested_and_18? method

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -29,9 +29,48 @@ class ColoradoLotteryTest < Minitest::Test
 
     assert_equal ({}), lottery.current_contestants
   end
+
+  def test_interested_and_18?
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    alexander = Contestant.new({
+                            first_name: 'Alexander',
+                            last_name: 'Aigades',
+                            age: 28,
+                            state_of_residence: 'CO',
+                            spending_money: 10})
+    benjamin = Contestant.new({
+                            first_name: 'Benjamin',
+                         last_name: 'Franklin',
+                            age: 17,
+                            state_of_residence: 'PA',
+                            spending_money: 100})
+    frederick = Contestant.new({
+                            first_name:  'Frederick',
+                            last_name: 'Douglas',
+                            age: 55,
+                            state_of_residence: 'NY',
+                            spending_money: 20})
+    winston = Contestant.new({
+                          first_name: 'Winston',
+                          last_name: 'Churchill',
+                          age: 18,
+                          state_of_residence: 'CO',
+                          spending_money: 5})
+
+    alexander.add_game_interest('Pick 4')
+    alexander.add_game_interest('Mega Millions')
+    frederick.add_game_interest('Mega Millions')
+    winston.add_game_interest('Cash 5')
+    winston.add_game_interest('Mega Millions')
+    benjamin.add_game_interest('Mega Millions')
+
+    assert_equal true,  lottery.interested_and_18?(alexander, pick_4)
+    assert_equal false, lottery.interested_and_18?(benjamin, mega_millions)
+    assert_equal false, lottery.interested_and_18?(alexander, cash_5)
+  end
 end
-
-
 
 
 # ### Iteration 2
@@ -40,19 +79,7 @@ end
 #
 # - A contestant #can_register? if they are interested in the game, 18 years of age
 # or older, and they are either a Colorado resident or this is a national game
-#
-#
-# #=> #<ColoradoLottery:0x007f8a3250c440...>
-#
-#lottery.registered_contestants
-# #=> {}
-#
-#lottery.winners
-# #=> []
-#
-#lottery.current_contestants
-# #=> {}
-#
+
 #pick_4 = Game.new('Pick 4', 2)
 # #=> #<Game:0x007f8a317b5e40...>
 #
