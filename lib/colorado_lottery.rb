cd6367050494f4eedfd40292a1c0cc3a40924439
Melanie Tran <melaniexx02@gmail.com>
2020-04-08 10:11:05 -0600

    Added interests and 18 method

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -1,9 +1,17 @@
 class ColoradoLottery
   attr_reader :registered_contestants, :winners, :current_contestants
-  
+
   def initialize
     @registered_contestants = {}
     @winners = []
     @current_contestants = {}
+  end
+
+  def interested_and_18?(contestant, game)
+    if contestant.game_interests.include?(game.name) && contestant.age > 18
+      true
+    else
+      false
+    end
   end
 end
