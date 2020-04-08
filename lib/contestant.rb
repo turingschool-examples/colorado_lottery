6c27fd9e23e6a146b01021541963e7474239e352
Melanie Tran <melaniexx02@gmail.com>
2020-04-08 09:16:07 -0600

    Created attr_reader for contest

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,4 +1,11 @@
 class Contestant
+  attr_reader :first_name, :last_name, :age, :spending_money, :state_of_residence
+  
   def initialize(info)
+    @first_name = info[:first_name]
+    @last_name = info[:last_name]
+    @age = info[:age]
+    @spending_money = info[:spending_money]
+    @state_of_residence = info[:state_of_residence]
   end
 end
