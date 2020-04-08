80227f5711f105045755e9b98300e53fbd6e604b
michelle <foleymichelle9@gmail.com>
2020-04-08 09:24:47 -0600

    age test passes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,8 +1,9 @@
 class Contestant
 
-  attr_reader :full_name
+  attr_reader :full_name, :age
 
   def initialize(attributes)
     @full_name = attributes[:first_name] + " " + attributes[:last_name]
+    @age = attributes[:age]
   end
 end
