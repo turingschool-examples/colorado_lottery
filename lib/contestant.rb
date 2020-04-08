b02e11205e60fa005381a9e2a7e17b33c13e6e59
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-08 05:18:26 -1000

    initialize full name variable

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,12 +1,15 @@
 class Contestant
 
-  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
+  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money, :full_name
 
   def initialize(info_hash)
     @first_name = info_hash[:first_name]
     @last_name = info_hash[:last_name]
+    @full_name = "#{@first_name} #{@last_name}"
     @age = info_hash[:age]
     @state_of_residence = info_hash[:state_of_residence]
     @spending_money = info_hash[:spending_money]
   end
+
+
 end
