f44650ad32fdba9805ed62f25874952093397587
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-08 09:05:21 -0600

    Pass test_returns_if_national_drawing

--- lib/game.rb
+++ lib/game.rb
@@ -5,4 +5,8 @@ class Game
     @cost = cost
     @national_drawing = national_drawing
   end
+
+  def national_drawing?
+    @national_drawing
+  end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -17,4 +17,9 @@ class GameTest < Minitest::Test
     assert_equal 5, @mega_millions.cost
     assert_equal true, @mega_millions.national_drawing
   end
+
+  def test_returns_if_national_drawing
+    assert_equal true, @mega_millions.national_drawing?
+    assert_equal false, @pick_4.national_drawing?
+  end
 end
