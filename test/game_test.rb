4c91366eacbdee135ae12c799e3a7be43f507a53
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-08 09:45:02 -0600

    add attributes and national_drawing? method to game class, passes tests

--- test/game_test.rb
+++ test/game_test.rb
@@ -20,22 +20,3 @@ class GameTest < Minitest::Test
     assert_equal false, @pick_4.national_drawing?
   end
 end
-
-
-# pry(main)> pick_4 = Game.new('Pick 4', 2)
-# #=> #<Game:0x007f96c296b7b0...>
-#
-# pry(main)> mega_millions = Game.new('Mega Millions', 5, true)
-# #=> #<Game:0x007f96c2953278...>
-#
-# pry(main)> mega_millions.name
-# #=> "Mega Millions"
-#
-# pry(main)> mega_millions.cost
-# #=> 5
-#
-# pry(main)> mega_millions.national_drawing?
-# #=> true
-#
-# pry(main)> pick_4.national_drawing?
-# #=> false
