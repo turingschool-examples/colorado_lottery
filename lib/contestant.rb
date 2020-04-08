555477a630f433795a43c98320b47221b5ccc646
michelle <foleymichelle9@gmail.com>
2020-04-08 09:31:18 -0600

    write out of state test and it  passes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,10 +1,15 @@
 class Contestant
 
-  attr_reader :full_name, :age, :state_of_residence
+  attr_reader :full_name, :age, :state_of_residence, :spending_money
 
   def initialize(attributes)
     @full_name = attributes[:first_name] + " " + attributes[:last_name]
     @age = attributes[:age]
     @state_of_residence = attributes[:state_of_residence]
+    @spending_money = attributes[:spending_money]
+  end
+
+  def out_of_state?
+    return false if @state_of_residence == "CO"
   end
 end
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -23,4 +23,8 @@ class ContestantTest < Minitest::Test
     assert_equal "CO", @alexander.state_of_residence
     assert_equal 10, @alexander.spending_money
   end
+
+  def test_it_is_not_out_of_state
+    assert_equal false, @alexander.out_of_state?
+  end
 end
