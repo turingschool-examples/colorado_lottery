d40f27f6c80c2b3896087a4cb8c426ab4d27bfdd
Melanie Tran <melaniexx02@gmail.com>
2020-04-08 09:22:16 -0600

    Created method for full name

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,6 +1,6 @@
 class Contestant
   attr_reader :first_name, :last_name, :age, :spending_money, :state_of_residence
-  
+
   def initialize(info)
     @first_name = info[:first_name]
     @last_name = info[:last_name]
@@ -8,4 +8,10 @@ class Contestant
     @spending_money = info[:spending_money]
     @state_of_residence = info[:state_of_residence]
   end
+
+  def full_name
+    @first_name + " " + @last_name
+  end
+
+  
 end
