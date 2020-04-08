d7bc260ca93d122550e4f2d895158bf73d600f5b
Ross Perry <perryr16@gmail.com>
2020-04-08 09:02:29 -0600

    test it exists

---
+++ test/game_test.rb
@@ -0,0 +1,68 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+require './lib/game'
+
+
+class GameTest < Minitest::Test
+
+  def setup
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @pick4
+    assert_instance_of Game, @mega_millions
+  end
+
+
+end
+
+#
+# #=> #<Game:0x007f96c296b7b0...>
+#
+# #=> #<Game:0x007f96c2953278...>
+#
+#mega_millions.name
+# #=> "Mega Millions"
+#
+#mega_millions.cost
+# #=> 5
+#
+#mega_millions.national_drawing?
+# #=> true
+#
+#pick_4.national_drawing?
+# #=> false
+#
+#alexander = Contestant.new({first_name: 'Alexander',
+#                                       last_name: 'Aigiades',
+#                                       age: 28,
+#                                       state_of_residence: 'CO',
+#                                       spending_money: 10})
+# #=> <Contestant:0x007ff87ac0a498...>
+#
+#alexander.full_name
+# #=> "Alexander Aigiades"
+#
+#alexander.age
+# #=> 28
+#
+#alexander.state_of_residence
+# #=> "CO"
+#
+#alexander.spending_money
+# #=> 10
+#
+#alexander.out_of_state?
+# #=> false
+#
+#alexander.game_interests
+# #=> []
+#
+#alexander.add_game_interest('Mega Millions')
+#alexander.add_game_interest('Pick 4')
+#
+#alexander.game_interests
+# => ["Mega Millions", "Pick 4"]
