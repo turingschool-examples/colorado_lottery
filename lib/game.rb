25c971f75666fa8a176b357e6426da0e12a7dae0
Mad Hallé <mad.halle@icloud.com>
2020-04-08 09:15:34 -0600

    game class initialized, national drawing method written

--- lib/game.rb
+++ lib/game.rb
@@ -1,3 +1,12 @@
 class Game
+ attr_reader :name, :cost, :national_drawing
+ def initialize(name, cost, national_drawing = false)
+   @name = name
+   @cost = cost
+   @national_drawing = national_drawing
+ end
 
-end 
+ def national_drawing?
+  @national_drawing == true
+ end
+end
