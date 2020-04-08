152f364806e427f152ed96be6f8a9a1d0215af24
Antonio Jackson <antoniojackson@gmail.com>
2020-04-08 09:19:49 -0600

    Adding attributes to Contestant class to pass first two tests

---
+++ lib/contestant.rb
@@ -0,0 +1,21 @@
+class Contestant
+
+  attr_reader :age, :state_of_residence, :spending_money, :game_interests
+
+  def initialize(contestant)
+    @first_name = contestant[:first_name]
+    @last_name= contestant[:last_name]
+    @age = contestant[:age]
+    @state_of_residence = contestant[:state_of_residence]
+    @spending_money = contestant[:spending_money]
+    @game_interests = []
+  end
+
+  def full_name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def out_of_state?
+    @state_of_residence != "CO"
+  end
+end
