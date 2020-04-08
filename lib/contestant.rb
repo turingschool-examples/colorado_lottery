722a2170ac46c687acdacd3b9d1c0254433f3118
michelle <foleymichelle9@gmail.com>
2020-04-08 09:33:17 -0600

    interests test passes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,12 +1,13 @@
 class Contestant
 
-  attr_reader :full_name, :age, :state_of_residence, :spending_money
+  attr_reader :full_name, :age, :state_of_residence, :spending_money, :game_interests
 
   def initialize(attributes)
     @full_name = attributes[:first_name] + " " + attributes[:last_name]
     @age = attributes[:age]
     @state_of_residence = attributes[:state_of_residence]
     @spending_money = attributes[:spending_money]
+    @game_interests = []
   end
 
   def out_of_state?
