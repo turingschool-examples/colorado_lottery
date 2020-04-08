162fe46fa60f1cfbd17382327e1bead4377adae0
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-08 05:38:02 -1000

    initialize with variables

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -1,3 +1,10 @@
 class ColoradoLottery
+  attr_reader :registered_contestants, :current_contestants, :winners
+
+  def initialize()
+    @registered_contestants = {}
+    @current_contestants = {}
+    @winners = []
+  end
 
 end
