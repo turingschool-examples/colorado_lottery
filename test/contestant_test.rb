4d4234a0c1566eb08d04ec4555a01a8392f35498
Derek Borski <derek.borski@gmail.com>
2020-04-08 09:20:00 -0600

    Adds full_name test

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -25,7 +25,17 @@ class ContestantTest < Minitest::Test
     assert_equal "Aigiades", alexander.last_name
     assert_equal 28, alexander.age
     assert_equal "CO", alexander.state_of_residence
-    assert_equal 10, alexander.spending_money                  
+    assert_equal 10, alexander.spending_money
+  end
+
+  def test_full_name
+    alexander = Contestant.new({first_name: 'Alexander',
+                                last_name: 'Aigiades',
+                                age: 28,
+                                state_of_residence: 'CO',
+                                spending_money: 10})
+
+    assert_equal "Alexander Aigiades", alexander.full_name                            
   end
 
 
@@ -44,31 +54,7 @@ end
 #
 # Use TDD to create a `Game` and `Contestant` class that respond to the following interaction pattern:
 # A contestant is considered out of state if their state of residence is not Colorado.
-#
-# ```ruby
-# #=> true
-#
-# require "./lib/game"
-# #=> true
-#
-# pick_4 = Game.new('Pick 4', 2)
-# #=> #<Game:0x007f96c296b7b0...>
-#
-# mega_millions = Game.new('Mega Millions', 5, true)
-# #=> #<Game:0x007f96c2953278...>
-#
-# mega_millions.name
-# #=> "Mega Millions"
-#
-# mega_millions.cost
-# #=> 5
-#
-# mega_millions.national_drawing?
-# #=> true
-#
-# pick_4.national_drawing?
-# #=> false
-#
+
 # alexander = Contestant.new({first_name: 'Alexander',
 #                                       last_name: 'Aigiades',
 #                                       age: 28,
