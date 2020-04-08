2785b707e8198409dc04b6f21c472cef6a21d684
Derek Borski <derek.borski@gmail.com>
2020-04-08 09:08:06 -0600

    Initializes Contestant class

---
+++ lib/contestant.rb
@@ -0,0 +1,12 @@
+class Contestant
+
+  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
+  def initialize(init_hash)
+    @first_name = init_hash[:first_name]
+    @last_name = init_hash[:last_name]
+    @age = init_hash[:age]
+    @state_of_residence = init_hash[:state_of_residence]
+    @spending_money = init_hash[:spending_money]
+  end
+
+end
