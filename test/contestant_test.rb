d9034f78f037aa240e2d844c515419be0f2c1127
Antonio Jackson <antoniojackson@gmail.com>
2020-04-08 09:13:36 -0600

    Adding Contestant class tests

---
+++ test/contestant_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/contestant'
+
+class ContestantTest < Minitest::Test
+
+  def setup
+    @alexander = Contestant.new({ first_name: 'Alexander',
+                                  last_name: 'Aigiades',
+                                  age: 28,
+                                  state_of_residence: 'CO',
+                                  spending_money: 10})
+  end
+
+  def test_it_exists
+    assert_instance_of Contestant, @alexander
+  end
+
+  def test_it_has_attributes
+    assert_equal "Alexander Aigiades", @alexander.full_name
+    assert_equal 28, @alexander.age
+    assert_equal "CO", @alexander.state_of_residence
+    assert_equal 10, @alexander.spending_money
+    assert_equal false, @alexander.out_of_state?
+    assert_equal [], @alexander.game_interests
+  end
+
+  def test_add_game_interest
+    @alexander.add_game_interest('Mega Millions')
+    @alexander.add_game_interest('Pick 4')
+    assert_equal ["Mega Millions", "Pick 4"], @alexander.game_interests
+  end
+end
