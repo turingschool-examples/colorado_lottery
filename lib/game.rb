cf31e0942fb62610f221ede0937748b4fb32ada4
Kiera Allen <kieraaallen@gmail.com>
2020-04-08 09:18:43 -0600

    Test existence of Game class with default argument national_drawing

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,11 @@
 class Game
+  attr_reader :name,
+              :cost,
+              :national_drawing
+
+  def initialize(name, cost, national_drawing = false)
+    @name = name
+    @cost = cost
+    @national_drawing = national_drawing
+  end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -3,4 +3,14 @@ require 'minitest/pride'
 require './lib/game'
 
 class GameTest < Minitest::Test
+
+  def setup
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @pick_4
+    assert_instance_of Game, @mega_millions
+  end
 end
