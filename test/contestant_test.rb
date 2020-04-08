b51d1fbabf030859e59bef3d925a47bbd8089a51
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-08 05:07:38 -1000

    add other tests for attributes

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -47,4 +47,7 @@ class ConstantTest < MiniTest::Test
                                       state_of_residence: 'CO',
                                       spending_money: 10})
     assert_equal "Alexander Aigiades", alexander.full_name
-  end                               
+    assert_equal 28, alexander.age
+    assert_equal "CO", alexander.state_of_residence
+    assert_equal 10, alexander.spending_money
+  end
