5020962af06ff9c3980cdea757a80b93686d80e7
Taylor Keller <takeller17@gmail.com>
2020-04-08 09:13:45 -0600

    Add contestant class, and fix errors in test

---
+++ lib/contestant.rb
@@ -0,0 +1,12 @@
+class Contestant
+
+  attr_reader :full_name, :age, :state_of_residence, :spending_money
+  def initialize(args)
+    @full_name = args[:first_name] + " " + args[:last_name]
+    @age = args[:age]
+    @state_of_residence = args[:state_of_residence]
+    @spending_money = args[:spending_money]
+  end
+
+  
+end
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -1,15 +1,16 @@
 require 'minitest/autorun'
 require 'minitest/pride'
+require './lib/contestant'
 
 
 class ContestantTest < Minitest::Test
 
   def setup
-    @alexander = Contestant.new({first_name: 'Alexander',}
+    @alexander = Contestant.new({first_name: 'Alexander',
                                  last_name: 'Aigiades',
                                  age: 28,
                                  state_of_residence: 'CO',
-                                 spending_money: 10)}
+                                 spending_money: 10})
   end
 
   def test_it_exists
@@ -20,5 +21,6 @@ class ContestantTest < Minitest::Test
     assert_equal "Alexander Aigiades", @alexander.full_name
     assert_equal 28, @alexander.age
     assert_equal "CO", @alexander.state_of_residence
+    assert_equal 10, @alexander.spending_money
   end
 end
