f519d17f9894ba6a4dfa36275b09bd69a5e3b89f
Jack Gould <johnhilsmangould@gmail.com>
2020-04-08 10:35:07 -0600

    add in methods to verify if they are interested in games

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -9,12 +9,31 @@ class ColoradoLottery
     @current_contestants = {}
   end
 
+  def find_game_interest(name, game_interest)
+    name.game_interests.find do |game|
+        if game_interest.name == game
+         return true
+       else
+         return false
+        end
+    end
+  end
+
   def interested_and_18?(name, game_interest)
+    if name.age >= 18 || find_game_interest(name, game_interest) == true 
+      true
+    else
+      false
+    end
+  end
+
+  def can_register?(name, game)
     if name.age >= 18
       true
     else
       false
     end
   end
+
 
 end
--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -21,6 +21,23 @@ class GameTest < Minitest::Test
     assert_equal ({}), lottery.current_contestants
   end
 
+  def test_it_can_verify_game_interest
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    alexander = Contestant.new({
+                          first_name: 'Alexander',
+                          last_name: 'Aigades',
+                          age: 28,
+                          state_of_residence: 'CO',
+                          spending_money: 10})
+    alexander.add_game_interest('Pick 4')
+    lottery = ColoradoLottery.new
+
+    assert_equal true, lottery.find_game_interest(alexander, pick_4)
+    assert_equal false, lottery.find_game_interest(alexander, mega_millions)
+  end
+
   def test_it_can_verify_age_and_game_of_interest
     pick_4 = Game.new('Pick 4', 2)
     mega_millions = Game.new('Mega Millions', 5, true)
@@ -41,6 +58,7 @@ class GameTest < Minitest::Test
     alexander.add_game_interest('Pick 4')
     alexander.add_game_interest('Mega Millions')
     benjamin.add_game_interest('Mega Millions')
+
 
     assert_equal true, lottery.interested_and_18?(alexander, pick_4)
   end
@@ -67,8 +85,8 @@ class GameTest < Minitest::Test
     benjamin.add_game_interest('Mega Millions')
 
 
-    assert_equal true, alexander.can_register?
-    assert_equal false, benjamin.can_register?
+    assert_equal true, lottery.can_register?(alexander, pick_4)
+    assert_equal false, lottery.can_register?(benjamin, mega_millions)
   end
 
 
