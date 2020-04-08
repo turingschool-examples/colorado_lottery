6645d07fb4a725fc941479affc6053416a718bf6
Melanie Tran <melaniexx02@gmail.com>
2020-04-08 10:29:07 -0600

    Add can register method

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -2,13 +2,21 @@ class ColoradoLottery
   attr_reader :registered_contestants, :winners, :current_contestants
 
   def initialize
-    @registered_contestants = {}
+    @registered_contestants= {}
     @winners = []
     @current_contestants = {}
   end
 
   def interested_and_18?(contestant, game)
     if contestant.game_interests.include?(game.name) && contestant.age > 18
+      true
+    else
+      false
+    end
+  end
+
+  def can_register?(contestant, game)
+    if interested_and_18?(contestant, game) == true || contestant.out_of_state? == false
       true
     else
       false
