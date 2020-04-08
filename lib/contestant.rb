b69cef40a08ebda357005b4a3c82a1d2570451e7
Eric Hale <eric_hale@comcast.net>
2020-04-08 10:04:55 -0600

    Add attribute methods and out of state test

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,6 +1,18 @@
 class Contestant
+  attr_reader :first_name,
+              :last_name,
+              :full_name,
+              :age,
+              :state_of_residence,
+              :spending_money
 
   def initialize(contestant_info)
     @contestant_info = contestant_info
+    @first_name = contestant_info[:first_name]
+    @last_name = contestant_info[:last_name]
+    @full_name = contestant_info[:first_name] + " " + contestant_info[:last_name]
+    @age = contestant_info[:age]
+    @state_of_residence = contestant_info[:state_of_residence]
+    @spending_money = contestant_info[:spending_money]
   end
 end
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -26,4 +26,14 @@ class ContestantTest < Minitest::Test
     assert_equal 'CO', alexander.state_of_residence
     assert_equal 10 , alexander.spending_money
   end
+
+  def test_it_is_out_of_state
+    alexander = Contestant.new({first_name: 'Alexander',
+                                      last_name: 'Aigiades',
+                                      age: 28,
+                                      state_of_residence: 'CO',
+                                      spending_money: 10})
+
+    assert_equal false, alexander.out_of_state?
+  end
 end
