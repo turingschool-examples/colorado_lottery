d82fa03cbf7e5aa187e3237c90a6e0f305cb0b24
Jane Greene <janegreenecasa@gmail.com>
2020-04-08 09:34:35 -0600

    Added methods for attributes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,6 +1,15 @@
 class Contestant
+  attr_reader :age, :state_of_residence, :spending_money
   def initialize(argument)
     @first_name = argument[:first_name]
+    @last_name = argument[:last_name]
+    @age = argument[:age]
+    @state_of_residence = argument[:state_of_residence]
+    @spending_money = argument[:spending_money]
+  end
+
+  def full_name
+    @first_name + " " + @last_name
   end
 
 end
