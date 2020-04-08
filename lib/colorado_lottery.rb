a40f8304b49ed3504de0501d4366b4818575df1a
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-08 10:07:48 -0600

    Add initialize to pass test

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -1,3 +1,12 @@
 class ColoradoLottery
-
+  attr_reader :registered_contestants,
+              :winners,
+              :current_contestants
+  def initialize(registered_contestants = {},
+                winners = [],
+                current_contestants = {})
+    @registered_contestants = registered_contestants
+    @winners = winners
+    @current_contestants = current_contestants
+  end
 end
