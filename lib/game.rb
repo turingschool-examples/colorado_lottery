94c8e345b559c33d02a40a41068424d9baaf4feb
Sage Lee <sagemcklee@gmail.com>
2020-04-08 09:15:34 -0600

    Add national drawing method

--- lib/game.rb
+++ lib/game.rb
@@ -3,5 +3,10 @@ class Game
   def initialize(name, cost, national_drawing = false)
     @name = name
     @cost = cost
+    @national_drawing = national_drawing
+  end
+
+  def national_drawing?
+    @national_drawing
   end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -14,7 +14,15 @@ class GameTest < Minitest::Test
     pick_4 = Game.new('Pick 4', 2)
     mega_millions = Game.new('Mega Millions', 5, true)
     assert_equal "Mega Millions", mega_millions.name
-    assert_equal 5, mega_millions.cost 
+    assert_equal 5, mega_millions.cost
+  end
+
+  def test_it_has_no_national_drawing_by_default
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+  
+    assert_equal true, mega_millions.national_drawing?
+    assert_equal false, pick_4.national_drawing?
   end
 
       #assert_equal true,mega_millions.national_drawing?
