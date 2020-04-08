c6402d2ba816fdd5ce8fbcaf26ec113d91146ec0
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 10:00:22 -0600

    Add method can_register? and helper method

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -10,4 +10,16 @@ class ColoradoLottery
   def interested_and_18?(contestant, game)
     contestant.age >= 18 && contestant.game_interests.include?(game.name)
   end
+
+  def out_of_state_only_for_national(contestant, game)
+    if game.national_drawing? == false
+      contestant.out_of_state? == false
+    else
+      true
+    end
+  end
+
+  def can_register?(contestant, game)
+    interested_and_18?(contestant, game) && out_of_state_only_for_national(contestant, game)
+  end
 end
