c3a49d48c331c5f68257d4a1a69bd5befb91eef1
Ross Perry <perryr16@gmail.com>
2020-04-08 09:10:53 -0600

    built it exists

---
+++ lib/contestant.rb
@@ -0,0 +1,11 @@
+class Contestant
+
+  def initialize(details)
+    @first_name = details[:first_name]
+    @last_name = details[:last_name]
+    @age = details[:age]
+    @state_of_residence = details[:state_of_residence]
+    @spending_money = details[:spending_money]
+  end
+
+end
