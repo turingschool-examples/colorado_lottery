41845905562c1b0335b4032f06b7fbd4fd2b21a3
Mad Hallé <mad.halle@icloud.com>
2020-04-08 09:27:45 -0600

    iteration 1 complete

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,11 +1,17 @@
 class Contestant
- attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
+ attr_reader :first_name,
+             :last_name,
+             :age,
+             :state_of_residence,
+             :spending_money,
+             :game_interests
   def initialize(args)
     @first_name = args[:first_name]
     @last_name = args[:last_name]
     @age = args[:age]
     @state_of_residence = args[:state_of_residence]
     @spending_money = args[:spending_money]
+    @game_interests = []
   end
 
   def full_name
@@ -14,5 +20,9 @@ class Contestant
 
   def out_of_state?
     @state_of_residence != "CO"
+  end
+
+  def add_game_interest(game_name)
+   @game_interests << game_name
   end
 end
