338bc5214ae609cd3a54416415a7fdb8168b18d5
michelle <foleymichelle9@gmail.com>
2020-04-08 09:20:08 -0600

    set up contestant class

---
+++ lib/contestant.rb
@@ -0,0 +1,5 @@
+class Contestant
+
+  def initialize(attributes)
+  end
+end
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -6,9 +6,14 @@ require "./lib/game"
 class ContestantTest < Minitest::Test
 
   def setup
-
+    @alexander = Contestant.new({first_name: 'Alexander',
+                                      last_name: 'Aigiades',
+                                      age: 28,
+                                      state_of_residence: 'CO',
+                                      spending_money: 10})
   end
 
   def test_it_exists
+    assert_instance_of Contestant, @alexander
   end
 end
