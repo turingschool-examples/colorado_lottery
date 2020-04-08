911b8b1c9a3078fbaaedddbb5c103156d1ff5425
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-08 09:12:30 -0600

    Update and pass test_it_has_attributes with game_interests

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,11 +1,13 @@
 class Contestant
-  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
+  attr_reader :first_name, :last_name, :age,
+              :state_of_residence, :spending_money, :game_interests
   def initialize(attributes)
     @first_name = attributes[:first_name]
     @last_name = attributes[:last_name]
     @age = attributes[:age]
     @state_of_residence = attributes[:state_of_residence]
     @spending_money = attributes[:spending_money]
+    @game_interests = []
   end
 
   def out_of_state?
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -21,9 +21,14 @@ class ContestantTest < Minitest::Test
     assert_equal 28, @alexander.age
     assert_equal 'CO', @alexander.state_of_residence
     assert_equal 10, @alexander.spending_money
+    assert_equal [], @alexander.game_interests
   end
 
   def test_returns_if_out_of_state
     assert_equal false, @alexander.out_of_state?
+  end
+
+  def test_can_add_game_interests
+
   end
 end
