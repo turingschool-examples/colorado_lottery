e4b287f32750b4415da50b4e7079782020149900
Alex Pariseau <arpariseau@gmail.com>
2020-04-08 09:11:44 -0600

    First tests for game

---
+++ test/game_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game'
+
+class GameTest < MiniTest::Test
+
+  def setup
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  def test_existence
+    assert_instance_of Game, @mega_millions
+  end
+
+  def test_attributes
+    assert_equal "Mega Millions", @mega_millions.name
+    assert_equal 5, @mega_millions.cost
+  end
+
+end
