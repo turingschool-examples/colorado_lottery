e9ee94071bafb4cbfbbbba5e4e5fe88813d8e94f
Derek Borski <derek.borski@gmail.com>
2020-04-08 09:31:15 -0600

    Creates add_game_interest method

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,12 +1,15 @@
 class Contestant
 
-  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
+  attr_reader :first_name, :last_name, :age, :state_of_residence,
+              :spending_money, :game_interests
+
   def initialize(init_hash)
     @first_name = init_hash[:first_name]
     @last_name = init_hash[:last_name]
     @age = init_hash[:age]
     @state_of_residence = init_hash[:state_of_residence]
     @spending_money = init_hash[:spending_money]
+    @game_interests = []
   end
 
   def full_name
@@ -15,5 +18,9 @@ class Contestant
 
   def out_of_state?
     @state_of_residence != "CO"
+  end
+
+  def add_game_interest(game_interest)
+    @game_interests << game_interest
   end
 end
