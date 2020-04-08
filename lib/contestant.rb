a018b0d74b1f29213eebe7be800a9c017dcf7ed9
Jane Greene <janegreenecasa@gmail.com>
2020-04-08 09:43:39 -0600

    Added method for game interests

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,11 +1,12 @@
 class Contestant
-  attr_reader :age, :state_of_residence, :spending_money
+  attr_reader :age, :state_of_residence, :spending_money, :game_interests
   def initialize(argument)
     @first_name = argument[:first_name]
     @last_name = argument[:last_name]
     @age = argument[:age]
     @state_of_residence = argument[:state_of_residence]
     @spending_money = argument[:spending_money]
+    @game_interests = []
   end
 
   def full_name
@@ -14,6 +15,10 @@ class Contestant
 
   def out_of_state?
     return false if @state_of_residence == "CO"
+  end
+
+  def add_game_interest(game)
+    @game_interests << game
   end
 
 end
