1fdd482ffbfb5b620a8140619d636316d366857c
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 09:20:29 -0600

    Add test for find if out of state

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -19,5 +19,9 @@ class ContestantTest < Minitest::Test
     assert_equal 28, @alexander.age
     assert_equal "CO", @alexander.state_of_residence
     assert_equal 10, @alexander.spending_money
-  end 
+  end
+
+  def test_it_can_find_out_of_state
+    assert_equal false, @alexander.out_of_state?
+  end
 end
