2f529af699113756b061c747f2ccbaa74129b40a
Mad Hallé <mad.halle@icloud.com>
2020-04-08 09:11:51 -0600

    test attribute and instance of

--- test/game_test.rb
+++ test/game_test.rb
@@ -8,25 +8,23 @@ class GameTest < Minitest::Test
     @pick_4 = Game.new('Pick 4', 2)
     @mega_millions = Game.new('Mega Millions', 5, true)
   end
+
+  def test_it_exists
+    assert_instance_of Game, @pick_4
+    assert_instance_of Game, @mega_millions
+  end
+
+  def test_it_has_attributes
+    assert_equal "Mega Millions", @mega_millions.name
+    assert_equal 5, @mega_millions.cost
+    assert_equal true, @mega_millions.national_drawing?
+    assert_equal false, @pick_4.national_drawing?
+  end
 end
 
-# pry(main)> pick_4 = Game.new('Pick 4', 2)
-# #=> #<Game:0x007f96c296b7b0...>
-#
-# pry(main)> mega_millions = Game.new('Mega Millions', 5, true)
-# #=> #<Game:0x007f96c2953278...>
-#
-# pry(main)> mega_millions.name
-# #=> "Mega Millions"
-#
-# pry(main)> mega_millions.cost
-# #=> 5
-#
-# pry(main)> mega_millions.national_drawing?
-# #=> true
-#
-# pry(main)> pick_4.national_drawing?
-# #=> false
+
+
+
 #
 # pry(main)> alexander = Contestant.new({first_name: 'Alexander',
 #                                       last_name: 'Aigiades',
