2c7b32a8543426f24b7c87a4583e7e17f4f68828
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 09:31:35 -0600

    Test attributes

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -14,5 +14,11 @@ class ColoradoLotteryTest < Minitest::Test
   def test_it_exists
     assert_instance_of ColoradoLottery, @lottery
   end
-  
+
+  def test_it_has_attributes
+    assert_equal ({}), @lottery.registered_contestants
+    assert_equal [], @lottery.winners
+    assert_equal ({}), @lottery.current_contestants
+  end
+
 end
