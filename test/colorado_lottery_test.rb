15e6c181b8bcd0bcb9d8c6a032b7b171b23b5ae9
Jane Greene <janegreenecasa@gmail.com>
2020-04-08 10:07:05 -0600

    Added test for interested and 18

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -6,6 +6,32 @@ require './lib/colorado_lottery'
 
 
 class ColoradoLotteryTest < Minitest::Test
+  def setup
+    @alexander = Contestant.new({
+                           first_name: 'Alexander',
+                           last_name: 'Aigades',
+                           age: 28,
+                           state_of_residence: 'CO',
+                           spending_money: 10})
+    @benjamin = Contestant.new({
+                            first_name: 'Benjamin',
+                            last_name: 'Franklin',
+                            age: 17,
+                            state_of_residence: 'PA',
+                            spending_money: 100})
+    @frederick = Contestant.new({
+                           first_name:  'Frederick',
+                           last_name: 'Douglas',
+                           age: 55,
+                           state_of_residence: 'NY',
+                           spending_money: 20})
+     @winston = Contestant.new({
+                          first_name: 'Winston',
+                          last_name: 'Churchill',
+                          age: 18,
+                          state_of_residence: 'CO',
+                          spending_money: 5})
+  end
   def test_it_exists
     lottery = ColoradoLottery.new
     assert_instance_of ColoradoLottery, lottery
@@ -16,5 +42,19 @@ class ColoradoLotteryTest < Minitest::Test
     assert_equal ({}), lottery.registered_contestants
     assert_equal [], lottery.winners
     assert_equal ({}), lottery.current_contestants
+  end
+
+  def test_interested_and_18
+    lottery = ColoradoLottery.new
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    @alexander.add_game_interest('Pick 4')
+    @alexander.add_game_interest('Mega Millions')
+    @frederick.add_game_interest('Mega Millions')
+    @winston.add_game_interest('Cash 5')
+    @winston.add_game_interest('Mega Millions')
+    @benjamin.add_game_interest('Mega Millions')
+    assert_equal true, lottery.interested_and_18?(@alexander, pick_4)
   end
 end
