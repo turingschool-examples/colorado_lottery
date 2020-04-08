c57a92c377b0600e981d8241afe6b0276d53994d
michelle <foleymichelle9@gmail.com>
2020-04-08 09:53:09 -0600

    set up contestants

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -9,6 +9,33 @@ class ColoradoLotteryTest < Minitest::Test
 
   def setup
     @lottery = ColoradoLottery.new
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+    @cash_5 = Game.new('Cash 5', 1)
+    @alexander = Contestant.new({
+                       first_name: 'Alexander',
+                       last_name: 'Aigades',
+                       age: 28,
+                       state_of_residence: 'CO',
+                       spending_money: 10})
+    @benjamin = Contestant.new({
+                       first_name: 'Benjamin',
+                       last_name: 'Franklin',
+                       age: 17,
+                       state_of_residence: 'PA',
+                       spending_money: 100})
+    @frederick = Contestant.new({
+                       first_name:  'Frederick',
+                       last_name: 'Douglas',
+                       age: 55,
+                       state_of_residence: 'NY',
+                       spending_money: 20})
+    @winston = Contestant.new({
+                     first_name: 'Winston',
+                     last_name: 'Churchill',
+                     age: 18,
+                     state_of_residence: 'CO',
+                     spending_money: 5})
   end
 
   def test_it_exists
@@ -20,5 +47,8 @@ class ColoradoLotteryTest < Minitest::Test
     assert_equal [], @lottery.winners
     assert_equal ({}), @lottery.current_contestants
   end
+
+  def test_it_add_game_interest
+  end 
 
 end
