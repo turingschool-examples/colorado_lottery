fac9b68b3e62b209444cc865a87aecec6c57a928
Alex Pariseau <arpariseau@gmail.com>
2020-04-08 09:33:14 -0600

    Contestant class iter 1

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,7 +1,8 @@
 class Contestant
   attr_reader :full_name, :age,
               :state_of_residence,
-              :spending_money
+              :spending_money,
+              :game_interests
 
   def initialize(contestant_info)
     @full_name = contestant_info[:first_name] + " " +
@@ -9,10 +10,15 @@ class Contestant
     @age = contestant_info[:age]
     @state_of_residence = contestant_info[:state_of_residence]
     @spending_money = contestant_info[:spending_money]
+    @game_interests = []
   end
 
   def out_of_state?
     @state_of_residence != "CO"
+  end
+
+  def add_game_interest(game_name)
+    @game_interests << game_name
   end
 
 end
