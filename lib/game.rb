d8cc406c79d837a97be4b10b0668e83bcfe25162
Eric Hale <eric_hale@comcast.net>
2020-04-08 09:14:13 -0600

    Add initialize method and attributes test

---
+++ lib/game.rb
@@ -0,0 +1,7 @@
+class Game
+
+  def initialize(name, cost)
+    @name = name
+    @cost = cost
+  end
+end
--- test/game_test.rb
+++ test/game_test.rb
@@ -8,4 +8,15 @@ class GameTest < Minitest::Test
     pick_4 = Game.new('Pick 4', 2)
     assert_instance_of Game, pick_4
   end
+
+  def test_it_has_attributes
+    mega_millions = Game.new('Mega Millions', 5, true)
+    pick_4 = Game.new('Pick 4', 2)
+    assert_equal 'Mega Millions', mega_millions.name
+    assert_equal 'Pick 4' , pick_4.name
+    assert_equal 5 , mega_millions.cost
+    assert_equal 2 , pick_4.cost
+    assert_equal true , mega_millions.national_drawing?
+    assert_equal false , pick_4.national_drawing?
+  end
 end
