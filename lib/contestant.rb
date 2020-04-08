72fe52acb941b519761e9ad475c3c2c39af48c95
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 09:19:16 -0600

    Add attr_reader and create instance variable for full_name

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,10 +1,12 @@
 class Contestant
+  attr_reader :first_name, :last_name, :full_name, :age, :state_of_residence, :spending_money
 
   def initialize(contestant_info)
     @first_name = contestant_info[:first_name]
     @last_name = contestant_info[:last_name]
+    @full_name = @first_name + " " + @last_name
     @age = contestant_info[:age]
     @state_of_residence = contestant_info[:state_of_residence]
     @spending_money = contestant_info[:spending_money]
   end
-end 
+end
