15bf1b4d141a98343d123df4d00647fbd1a9f328
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-08 05:23:15 -1000

    add requires and minitest

---
+++ test/colorado_lottery_test.rb
@@ -0,0 +1,11 @@
+require "./lib/contestant"
+require "./lib/game"
+require 'colorado_lottery'
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'pry'
+
+class ColoradoLotteryTest < MiniTest::Test
+
+
+end
