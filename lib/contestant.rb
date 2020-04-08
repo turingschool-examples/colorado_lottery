8ab63570e7743bd456b1147c1855388b1bf32c2b
Sage Lee <sagemcklee@gmail.com>
2020-04-08 09:35:01 -0600

    Add test it can add game interests

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,5 +1,11 @@
 class Contestant
+  attr_reader :full_name, :age, :state_of_residence, :spending_money
   def initialize(info)
-  
+  @first_name = info[:first_name]
+  @last_name = info[:last_name]
+  @full_name = info[:first_name] + " " + info[:last_name]
+  @age = info[:age]
+  @state_of_residence = info[:state_of_residence]
+  @spending_money = info[:spending_money]
   end
 end
