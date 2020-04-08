a3c7cf690abc1066042733fea5fff6ca9fef7d54
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-08 09:17:33 -0600

    Pass test_it_has_attributes

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -1,2 +1,8 @@
 class ColoradoLottery
+  attr_reader :registered_contestants, :winners, :current_contestants
+  def initialize
+    @registered_contestants = {}
+    @winners = []
+    @current_contestants = {}
+  end
 end
