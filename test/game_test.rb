7e3de9fead5967ba11c816cb88dbc48e71d16ab4
Antonio Jackson <antoniojackson@gmail.com>
2020-04-08 09:05:45 -0600

    Initial commit with game tests

---
+++ test/game_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game'
+
+class GameTest < Minitest::Test
+
+  def setup
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @pick_4
+  end
+
+  def test_it_has_attributes
+    assert_equal "Mega Millions", @mega_millions.name
+    assert_equal 5, @mega_millions.cost
+    assert_equal true, @mega_millions.national_drawing?
+    assert_equal false, @pick_4.national_drawing?
+  end
+end
