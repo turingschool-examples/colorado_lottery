42d56b4f288b3502ef305593c0d3e5969db05c33
Taylor Keller <takeller17@gmail.com>
2020-04-08 09:19:53 -0600

    Initialize game_interests as empty array

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,14 +1,17 @@
 class Contestant
 
-  attr_reader :full_name, :age, :state_of_residence, :spending_money
+  attr_reader :full_name, :age, :state_of_residence, :spending_money,
+  :game_interests
   def initialize(args)
     @full_name = args[:first_name] + " " + args[:last_name]
     @age = args[:age]
     @state_of_residence = args[:state_of_residence]
     @spending_money = args[:spending_money]
+    @game_interests = []
   end
 
   def out_of_state?
     @state_of_residence != "CO"
   end
+
 end
