b855cc9c4dba126dcb03abae952b4f0aa4b677a0
Melanie Tran <melaniexx02@gmail.com>
2020-04-08 09:37:04 -0600

    Created test it exists

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -5,4 +5,11 @@ require './lib/contestant'
 require './lib/colorado_lottery'
 
 class ColoradoLotteryTest < Minitest::Test
+  def setup
+    @lottery = ColoradoLottery.new
+  end
+
+  def test_it_exists
+    assert_instance_of ColoradoLottery, @lottery
+  end
 end
