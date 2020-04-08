48b28954ebf718bb2cc5028f47555ab954ef644f
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 09:46:22 -0600

    Add method to find if interested and over 18

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -6,4 +6,8 @@ class ColoradoLottery
     @winners = []
     @current_contestants = {}
   end
+
+  def interested_and_18?(contestant, game)
+    contestant.age >= 18 && contestant.game_interests.include?(game.name)
+  end
 end
