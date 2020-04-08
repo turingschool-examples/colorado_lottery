e1fc25555b6244f71ba91c513241fff2b72202de
Mad Hallé <mad.halle@icloud.com>
2020-04-08 09:39:35 -0600

    methods written for tests colorado lottery class

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -1,1 +1,12 @@
-# - A contestant #can_register? if they are interested in the game, 18 years of age or older, and they are either a Colorado resident or this is a national game
+class ColoradoLottery
+  attr_reader :winners, :registered_contestants, :current_contestants
+  def initialize
+    @winners = []
+    @registered_contestants = {}
+    @current_contestants = {}
+  end
+end
+
+# - A contestant #can_register? if they are interested in the game,
+#18 years of age or older, and they are either a Colorado resident
+#or this is a national game
