dad9e6cd6db13efd5547fa42d57f9126849e7650
Norma Lopez <norma_l304@yahoo.com>
2020-04-08 09:36:19 -0600

    Add test for add game interest method

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -61,6 +61,21 @@ class ContestantTest < Minitest::Test
       spending_money: 10
     })
 
-    assert_equal [], alexander.game_inerests
+    assert_equal [], alexander.game_interests
   end
+
+  def test_contestant_can_have_game_interests
+    alexander = Contestant.new({
+      first_name: 'Alexander',
+      last_name: 'Aigiades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10
+    })
+
+    alexander.add_game_interest('Mega Millions')
+    alexander.add_game_interest('Pick 4')
+
+    assert_equal ["Mega Millions", "Pick 4"], alexander.game_interests
+  end 
 end
