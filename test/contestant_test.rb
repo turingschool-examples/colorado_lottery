44684245c9d3b116115730c1a3b2fd5a8df789b9
Derek Borski <derek.borski@gmail.com>
2020-04-08 09:07:49 -0600

    Creates tests for readable attributes

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -4,15 +4,29 @@ require "./lib/contestant"
 
 class ContestantTest < Minitest::Test
 
-def test_it_exists
-  alexander = Contestant.new({first_name: 'Alexander',
-                              last_name: 'Aigiades',
-                              age: 28,
-                              state_of_residence: 'CO',
-                              spending_money: 10})
+  def test_it_exists
+    alexander = Contestant.new({first_name: 'Alexander',
+                                last_name: 'Aigiades',
+                                age: 28,
+                                state_of_residence: 'CO',
+                                spending_money: 10})
 
-  assert_instance_of Contestant, alexander
-end
+    assert_instance_of Contestant, alexander
+  end
+
+  def test_it_has_readable_attributes
+    alexander = Contestant.new({first_name: 'Alexander',
+                                last_name: 'Aigiades',
+                                age: 28,
+                                state_of_residence: 'CO',
+                                spending_money: 10})
+
+    assert_equal "Alexander", alexander.first_name
+    assert_equal "Aigiades", alexander.last_name
+    assert_equal 28, alexander.age
+    assert_equal "CO", alexander.state_of_residence
+    assert_equal 10, alexander.spending_money                  
+  end
 
 
 end
