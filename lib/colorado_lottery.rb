0aac12999bbda35c3c87425a1a8903cf6d4c8982
Jack Cullen <jpcullen20@gmail.com>
2020-04-08 10:10:25 -0600

    Add register contestant method

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -19,4 +19,11 @@ class ColoradoLottery
     in_state_or_national = (!contestant.out_of_state? || game.national_drawing)
     interested_and_18?(contestant, game) && in_state_or_national
   end
+
+  def register_contestant(contestant, game)
+    if can_register?(contestant, game)
+      @registered_contestants[game.name] = [] if !@registered_contestants[game.name]
+      @registered_contestants[game.name] << contestant
+    end
+  end
 end
