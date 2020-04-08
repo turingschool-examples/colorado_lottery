900bf80ea774b249ed3c373511921aa1f7912eff
Jack Gould <johnhilsmangould@gmail.com>
2020-04-08 09:09:24 -0600

    test all readable attributes in game class

--- test/game_test.rb
+++ test/game_test.rb
@@ -13,4 +13,17 @@ class GameTest < Minitest::Test
     assert_instance_of Game, mega_millions
   end
 
+  def test_it_has_readable_attributes
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+
+
+    assert_equal 'Pick 4', pick_4.name
+    assert_equal 2, pick_4.cost
+    assert_equal false, pick_4.national_drawing
+    assert_equal 'Mega Millions', mega_millions.name
+    assert_equal 5, mega_millions.cost
+    assert_equal true, mega_millions.national_drawing
+  end
+
 end
