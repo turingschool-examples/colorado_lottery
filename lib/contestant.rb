019b09111e08cf7449ff602fa74e0384ef91d5ff
Eric Larson <ericlarsonbroom@gmail.com>
2020-04-08 09:16:14 -0600

    fixed tests so game_test tested right class

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,1 +1,11 @@
+class Contestant
+  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
+  def initialize(info)
+    @first_name = info[:first_name]
+    @last_name = info[:last_name]
+    @age = info[:age]
+    @state_of_residence = info[:state_of_residence]
+    @spending_money = info[:spending_money]
+  end
 
+end
