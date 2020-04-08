aee0f2b84685e9c8397b6bf5584e72e4ce781918
michelle <foleymichelle9@gmail.com>
2020-04-08 09:09:11 -0600

    set up class and tests

---
+++ test/contestant_test.rb
@@ -0,0 +1,14 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/contestant"
+require "./lib/game"
+
+class ContestantTest < Minitest::Test
+
+  def setup
+
+  end
+
+  def test_it_exists
+  end
+end
---
+++ test/game_test.rb
@@ -0,0 +1,20 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/contestant"
+require "./lib/game"
+
+class GameTest < Minitest::Test
+
+  def setup
+    @pick_4 = Game.new('Pick 4', 2)
+    #@mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @pick_4
+  end
+
+  def test_it_has_name
+    assert_equal "Mega Millions", @mega_millions.name
+  end
+end
