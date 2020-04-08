8a6af1a9cb40504a09e89e8f61e5550f019cb307
James Belta <james.belta@gmail.com>
2020-04-08 10:07:24 -0600

    Made setup for test

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -4,36 +4,30 @@ require './lib/contestant'
 
 class ContestantTest < Minitest::Test
 
-  def test_it_exist
-    alexander = Contestant.new({first_name: 'Alexander',
+  def setup
+    @alexander = Contestant.new({first_name: 'Alexander',
                                       last_name: 'Aigiades',
                                       age: 28,
                                       state_of_residence: 'CO',
                                       spending_money: 10})
-    assert_instance_of Contestant, alexander
+  end
+
+  def test_it_exist
+    assert_instance_of Contestant, @alexander
   end
 
   def test_it_has_attributes
-    alexander = Contestant.new({first_name: 'Alexander',
-                                      last_name: 'Aigiades',
-                                      age: 28,
-                                      state_of_residence: 'CO',
-                                      spending_money: 10})
-                                      #require 'pry'; binding.pry
-    assert_equal "Alexander", alexander.first_name
-    assert_equal "Aigiades", alexander.last_name
-    assert_equal 28, alexander.age
-    assert_equal 'CO', alexander.state_of_residence
-    assert_equal 10, alexander.spending_money
+    assert_equal "Alexander", @alexander.first_name
+    assert_equal "Aigiades", @alexander.last_name
+    assert_equal 28, @alexander.age
+    assert_equal 'CO', @alexander.state_of_residence
+    assert_equal 10, @alexander.spending_money
   end
 
   def test_full_name
-    alexander = Contestant.new({first_name: 'Alexander',
-                                      last_name: 'Aigiades',
-                                      age: 28,
-                                      state_of_residence: 'CO',
-                                      spending_money: 10})
-                                      #require 'pry'; binding.pry
-    assert_equal 'Alexander Aigiades', alexander.full_name
+    assert_equal 'Alexander Aigiades', @alexander.full_name
   end
+
+  #def test_out_of_state
+
 end
