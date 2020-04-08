4de57cd052a08fb9a3d6c29ed725832fbac07b26
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 09:25:43 -0600

    Create method to add game interests

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,5 +1,5 @@
 class Contestant
-  attr_reader :first_name, :last_name, :full_name, :age, :state_of_residence, :spending_money
+  attr_reader :first_name, :last_name, :full_name, :age, :state_of_residence, :spending_money, :game_interests
 
   def initialize(contestant_info)
     @first_name = contestant_info[:first_name]
@@ -8,9 +8,15 @@ class Contestant
     @age = contestant_info[:age]
     @state_of_residence = contestant_info[:state_of_residence]
     @spending_money = contestant_info[:spending_money]
+    @game_interests = []
   end
 
   def out_of_state?
     @state_of_residence != "CO"
   end
+
+  def add_game_interest(game)
+    @game_interests << game
+  end
+
 end
