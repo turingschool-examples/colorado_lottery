ce01e16a139ff619b8085baa23d128f601829c03
Norma Lopez <norma_l304@yahoo.com>
2020-04-08 09:19:09 -0600

    Add attributes test

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -11,6 +11,20 @@ class ContestantTest < Minitest::Test
       state_of_residence: 'CO',
       spending_money: 10
     })
-    assert_instance_of Contestant, alexander 
+    assert_instance_of Contestant, alexander
+  end
+
+  def test_it_has_attributes
+    alexander = Contestant.new({
+      first_name: 'Alexander',
+      last_name: 'Aigiades',
+      age: 28,
+      state_of_residence: 'CO',
+      spending_money: 10
+    })
+
+    assert_equal 28, alexander.age
+    assert_equal "CO", alexander.state_of_residence
+    assert_equal 10, alexander.spending_money
   end
 end
