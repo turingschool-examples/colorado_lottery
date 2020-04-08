5649f1e0154bf9451b4e8a0676edfb4430ddfd97
michelle <foleymichelle9@gmail.com>
2020-04-08 09:44:31 -0600

    set up class and test for existence

---
+++ lib/colorado_lottery.rb
@@ -0,0 +1,2 @@
+class ColoradoLottery
+end
---
+++ test/colorado_lottery_test.rb
@@ -0,0 +1,21 @@
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/contestant"
+require "./lib/game"
+require "./lib/colorado_lottery"
+
+
+class ColoradoLotteryTest < Minitest::Test
+
+  def setup
+    @lottery = ColoradoLottery.new
+  end
+
+  def test_it_exists
+    assert_instance_of ColoradoLottery, @lottery
+  end
+
+  def test_contestants
+    assert_equal ({}), @lottery.registered_contestants
+  end 
+end
