e25d841ebeea277dbb349cdbce02d684376309d5
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-08 09:09:43 -0600

    Pass test_it_has_attributes

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,5 +1,12 @@
 class Contestant
+  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
   def initialize(attributes)
-    @attributes = attributes
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @age = attributes[:age]
+    @state_of_residence = attributes[:state_of_residence]
+    @spending_money = attributes[:spending_money]
+
+
   end
 end
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -14,4 +14,12 @@ class ContestantTest < Minitest::Test
   def test_it_exists
     assert_instance_of Contestant, @alexander
   end
+
+  def test_it_has_attributes
+    assert_equal 'Alexander', @alexander.first_name
+    assert_equal 'Aigiades', @alexander.last_name
+    assert_equal 28, @alexander.age
+    assert_equal 'CO', @alexander.state_of_residence
+    assert_equal 10, @alexander.spending_money
+  end
 end
