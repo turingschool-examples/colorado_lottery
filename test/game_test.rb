e91bdd65caf4765689b92c5da948bd59350b77a4
Michael Gallup <Gallup93@icloud.com>
2020-04-08 09:07:48 -0600

    Added testing for game initializtion with attributes

---
+++ test/game_test.rb
@@ -0,0 +1,22 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game'
+
+class GameTest < Minitest::Test
+
+  def setup
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  def test_initialization_with_attributes
+    assert_instance_of Game, @pick_4
+    assert_equal 'Mega Millions', @mega_millions.name
+    assert_equal 5, @mega_millions.cost
+    assert_equal true, @mega_millions.national_drawing
+    assert_equal false, @pick_4.national_drawing
+  end
+
+
+
+end
