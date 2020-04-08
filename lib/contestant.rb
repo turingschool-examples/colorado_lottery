50e039aface3822682ce1180f6a8d5b67f4cfd1b
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-08 05:20:25 -1000

    initialize game_interests variable ad empty array for each contestant

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,6 +1,6 @@
 class Contestant
 
-  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money, :full_name
+  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money, :full_name, :game_interests
 
   def initialize(info_hash)
     @first_name = info_hash[:first_name]
@@ -9,6 +9,7 @@ class Contestant
     @age = info_hash[:age]
     @state_of_residence = info_hash[:state_of_residence]
     @spending_money = info_hash[:spending_money]
+    @game_interests = []
   end
 
   def out_of_state?
