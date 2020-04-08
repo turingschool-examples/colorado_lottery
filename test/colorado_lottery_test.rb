4835ce085598ab3003e5702d6fcf0e4146ead039
Judith Pillado <judith.pillado@gmail.com>
2020-04-08 11:31:07 -0500

    Add class

---
+++ test/colorado_lottery_test.rb
@@ -0,0 +1,8 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/contestant'
+require "./lib/game"
+require "./lib/colorado_lottery"
+
+class ColoradoLotteryTest < Minitest::Test
+end
