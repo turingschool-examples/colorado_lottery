a67b8a3744dd0ff4c559161b35f90ad8c93ce2b5
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 10:51:43 -0600

    Changed spending_money to attr_accessor

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,5 +1,6 @@
 class Contestant
-  attr_reader :first_name, :last_name, :full_name, :age, :state_of_residence, :spending_money, :game_interests
+  attr_reader :first_name, :last_name, :full_name, :age, :state_of_residence, :game_interests
+  attr_accessor :spending_money
 
   def initialize(contestant_info)
     @first_name = contestant_info[:first_name]
