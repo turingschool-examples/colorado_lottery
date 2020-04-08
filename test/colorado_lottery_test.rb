e7d7cdbb3796282627ad6bcc9efbcf9b168c8b00
Rostam Mahabadi <Rostammahabadi@gmail.com>
2020-04-08 05:31:07 -1000

    test contestant is interested and legal age

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -8,8 +8,58 @@ require 'pry'
 class ColoradoLotteryTest < MiniTest::Test
 
   def test_it_exists
+
+    assert_instance_of ColoradoLottery, ColoradoLottery.new
+  end
+
+  def test_it_has_attributes
     lottery = ColoradoLottery.new
-    assert_instance_of ColoradoLottery, lottery
+
+    assert_equal {}, lottery.registered_contestants
+    assert_equal {}, lottery.current_contestants
+    #assert_equal [], lottery.winners
+  end
+
+  def test_contestant_is_interested_and_legal_age
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    alexander = Contestant.new({
+                       first_name: 'Alexander',
+                       last_name: 'Aigades',
+                       age: 28,
+                       state_of_residence: 'CO',
+                       spending_money: 10})
+   benjamin = Contestant.new({
+                        first_name: 'Benjamin',
+                        last_name: 'Franklin',
+                        age: 17,
+                        state_of_residence: 'PA',
+                        spending_money: 100})
+    frederick = Contestant.new({
+                       first_name:  'Frederick',
+                       last_name: 'Douglas',
+                       age: 55,
+                       state_of_residence: 'NY',
+                       spending_money: 20})
+     winston = Contestant.new({
+                       first_name: 'Winston',
+                       last_name: 'Churchill',
+                       age: 18,
+                       state_of_residence: 'CO',
+                       spending_money: 5})
+     alexander.add_game_interest('Pick 4')
+     alexander.add_game_interest('Mega Millions')
+     frederick.add_game_interest('Mega Millions')
+     winston.add_game_interest('Cash 5')
+     winston.add_game_interest('Mega Millions')
+     benjamin.add_game_interest('Mega Millions')
+     assert_equal true, lottery.interested_and_18?(alexander, pick_4)
+     assert_equal false, lottery.interested_and_18?(benjamin, mega_millions)
+     assert_equal false, lottery.interested_and_18?(alexander, cash_5)
+     # assert_equal true, lottery.can_register?(alexander, pick_4)
+     # assert_equal false, lottery.can_register?(alexander, cash_5)
+     # assert_equal true, lottery.can_register?(frederick, mega_millions)
   end
 
 
