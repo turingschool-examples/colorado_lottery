8d3356fee0c610430598e1a3cddb7f6bd3ca4659
Ashkan Abbasi <airashkan@yahoo.com>
2020-04-08 09:14:39 -0600

    Create Game tests and class.

---
+++ test/contestant_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require "./lib/contestant"
+
+
+class ContestantTest < Minitest::Test
+  
+
+end
---
+++ test/game_test.rb
@@ -0,0 +1,26 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require "./lib/contestant"
+require "./lib/game"
+
+
+class GameTest < Minitest::Test
+
+  def setup
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @pick_4
+    assert_instance_of Game, @mega_millions
+  end
+
+  def test_game_attributes
+    assert_equal "Mega Millions", @mega_millions.name
+    assert_equal 5, @mega_millions.cost
+    assert_equal true, @mega_millions.national_drawing?
+    assert_equal false, @pick_4.national_drawing?
+  end
+
+end
