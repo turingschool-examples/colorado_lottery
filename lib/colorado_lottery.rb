837b503d5ea4a2468b465e9df3c81499ccf84036
stellakunzang <stella.k.bonnie@gmail.com>
2020-04-08 09:33:07 -0600

    Add instance variables to initialize

---
+++ lib/colorado_lottery.rb
@@ -0,0 +1,9 @@
+class ColoradoLottery
+  attr_reader :registered_contestants, :winners, :current_contestants
+
+  def initialize
+    @registered_contestants = {}
+    @winners = []
+    @current_contestants = {}
+  end
+end
