f8a32b0052921230d8ec24ef90a86d6fd2925e48
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-08 09:07:13 -0600

    Pass test_it_exists

---
+++ lib/contestant.rb
@@ -0,0 +1,5 @@
+class Contestant
+  def initialize(attributes)
+    @attributes = attributes
+  end
+end
---
+++ test/contestant_test.rb
@@ -0,0 +1,17 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/Contestant'
+
+class ContestantTest < Minitest::Test
+  def setup
+    @alexander = Contestant.new({ first_name: 'Alexander',
+                                  last_name: 'Aigiades',
+                                  age: 28,
+                                  state_of_residence: 'CO',
+                                  spending_money: 10})
+  end
+
+  def test_it_exists
+    assert_instance_of Contestant, @alexander
+  end
+end
