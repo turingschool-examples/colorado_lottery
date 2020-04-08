3494a6c5668b1e133c5621b0ed68c9499f0925b2
Kyle Iverson <kyleiversonlax@hotmail.com>
2020-04-08 09:42:01 -0600

    Add test for readable attributes

--- test/game_test.rb
+++ test/game_test.rb
@@ -12,6 +12,13 @@ class GameTest < Minitest::Test
     assert_instance_of Game, @pick_4
     assert_instance_of Game, @mega_millions
   end
+
+  def test_it_has_readable_attributes
+    assert_equal "Mega Millions", @mega_millions.name
+    assert_equal 5, @mega_millions.cost
+    assert_equal true, @mega_millions.national_drawing?
+    assert_equal false, @pick_4.national_drawing?
+  end
 end
 
 
