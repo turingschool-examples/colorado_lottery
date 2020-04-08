0a3a063e40de3ec281711fb01bd9971b64392860
Jack Gould <johnhilsmangould@gmail.com>
2020-04-08 09:16:41 -0600

    add game class and test methods

--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -1,7 +1,31 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/game'
+require './lib/contestant'
 require 'pry';
 
 
-class DockTest < Minitest::Test
+class ContestantTest < Minitest::Test
+  def test_it_exists
+    alexander = Contestant.new({first_name: 'Alexander',
+                                          last_name: 'Aigiades',
+                                          age: 28,
+                                          state_of_residence: 'CO',
+                                          spending_money: 10})
+
+    assert_instance_of Contestant, alexander
+  end
+
+  def test_it_has_readable_attributes
+    alexander = Contestant.new({first_name: 'Alexander',
+                                          last_name: 'Aigiades',
+                                          age: 28,
+                                          state_of_residence: 'CO',
+                                          spending_money: 10})
+    assert_equal 'Alexander', alexander.first_name
+    assert_equal 'Aigiades', alexander.last_name
+    assert_equal 28, alexander.age
+    assert_equal 'CO', alexander.state_of_residence
+    assert_equal 10, alexander.spending_money
+    assert_equal [], alexander.game_interests
+  end
+end
