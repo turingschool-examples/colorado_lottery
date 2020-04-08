0ac56347eb9bd50d2eceaff10e06b538adb82f08
Judith Pillado <judith.pillado@gmail.com>
2020-04-08 11:28:54 -0500

    Add national drawing

--- lib/game.rb
+++ lib/game.rb
@@ -1,2 +1,17 @@
 class Game
-end 
+  attr_reader :name, :cost, :national_drawing
+
+  def initialize(name, cost, national_drawing)
+    @name             = name
+    @cost             = cost
+    @national_drawing = true
+  end
+
+  def national_drawing?
+    if @national_drawing
+      true
+    else
+      false
+    end
+  end
+end
