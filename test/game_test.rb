2b9f9d04f0d27d1d31cd784b16f085c04d150046
Taylor Keller <takeller17@gmail.com>
2020-04-08 09:27:54 -0600

    Setup test file and added existance and attributes tests

---
+++ test/game_test.rb
@@ -0,0 +1,21 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/game'
+
+class GameTest < MiniTest::Test
+
+  def setup
+    @pick_4 = Game.new('Pick 4', 2)
+    @mega_millions = Game.new('Mega Millions', 5, true)
+  end
+
+  def test_it_exists
+    assert_instance_of Game, @pick4
+    assert_instance_of Game, @mega_millions
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "Mega Millions", @mega_millions.name
+    assert_equal 5, @mega_millions.cost
+  end
+end
