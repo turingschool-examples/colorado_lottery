6392df4a77bc50e83105cc0c8ef38ab24aa636fe
Norma Lopez <norma_l304@yahoo.com>
2020-04-08 10:19:28 -0600

    Add test for colorado lottery class

---
+++ test/colorado_lottery_test.rb
@@ -0,0 +1,13 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/contestant'
+require "./lib/game"
+require "./lib/colorado_lottery"
+
+class ColoradoLotteryTest < Minitest::Test
+  def test_it_exists
+    lottery = ColoradoLottery.new
+
+    assert_instance_of ColoradoLottery, lottery 
+  end
+end
