27af375447378f035a0eea3e5cbc65f5fad4ff06
Mad Hallé <mad.halle@icloud.com>
2020-04-08 09:24:55 -0600

    test out of state, test game interests

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -1,14 +1,18 @@
 class Contestant
  attr_reader :first_name, :last_name, :age, :state_of_residence, :spending_money
- def initialize(args)
-   @first_name = args[:first_name]
-   @last_name = args[:last_name]
-   @age = args[:age]
-   @state_of_residence = args[:state_of_residence]
-   @spending_money = args[:spending_money]
- end
+  def initialize(args)
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @age = args[:age]
+    @state_of_residence = args[:state_of_residence]
+    @spending_money = args[:spending_money]
+  end
 
- def full_name
-   @first_name + ' ' + @last_name
- end
+  def full_name
+    @first_name + ' ' + @last_name
+  end
+
+  def out_of_state?
+    @state_of_residence != "CO"
+  end
 end
--- test/contestant_test.rb
+++ test/contestant_test.rb
@@ -22,15 +22,21 @@ class ContestantTest < Minitest::Test
     assert_equal "CO", @alexander.state_of_residence
     assert_equal 10, @alexander.spending_money
   end
+
+  def test_out_of_state?
+   assert_equal false, @alexander.out_of_state?
+  end
+
+  def test_game_interests
+    assert_equal [], @alexander.game_interests
+  end
 end
 
 
 
 
 
-#
-# pry(main)> alexander.out_of_state?
-# #=> false
+
 #
 # pry(main)> alexander.game_interests
 # #=> []
