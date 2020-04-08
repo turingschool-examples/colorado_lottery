e112605e1aa24c0545dcea762e0ba8c0593d76ec
Antonio Jackson <antoniojackson@gmail.com>
2020-04-08 09:45:52 -0600

    Adding interested_and_18? and can_register? methods to pass tests

---
+++ lib/colorado_lottery.rb
@@ -0,0 +1,18 @@
+class ColoradoLottery
+
+  attr_reader :registered_contestants, :winners, :current_contestants
+
+  def initialize
+    @registered_contestants = {}
+    @winners = []
+    @current_contestants = {}
+  end
+
+  def interested_and_18?(contestant, game)
+    contestant.age >= 18 && contestant.game_interests.include?(game.name)
+  end
+
+  def can_register?(contestant, game)
+    interested_and_18?(contestant, game) && (contestant.out_of_state? == false || game.national_drawing? == true)
+  end
+end
