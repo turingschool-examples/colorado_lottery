8cca0c573a94b507a59ac752abeb709cb8a86ee6
michelle <foleymichelle9@gmail.com>
2020-04-08 09:17:18 -0600

    national drawing tests pass

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,14 @@
 class Game
 
-  attr_reader :name, :cost 
+  attr_reader :name, :cost, :national_drawing
 
   def initialize(name_parameter, cost, national_drawing)
     @name = name_parameter
     @cost = cost
+    @national_drawing =  national_drawing
+  end
+
+  def national_drawing?
+    @national_drawing
   end
 end
