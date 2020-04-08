8d40db3a8553fc9c2e4175d3145ff2ef03ebb138
Eric Hale <eric_hale@comcast.net>
2020-04-08 10:56:27 -0600

    Add Test for registered contestants

--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -10,4 +10,10 @@ class ColoradoLotteryTest < Minitest::Test
     lottery = ColoradoLottery.new
     assert_instance_of ColoradoLottery, lottery
   end
+
+  def test_it_has_registered_contestants
+    lottery = ColoradoLottery.new
+    #unable to resove error with testing
+    assert_equal {} , lottery.registered_contestants
+  end
 end
