aa6d0d3f399f3ba18faa04f2597a0b710bed1aa3
Whitney Kidd <whitneymkidd@gmail.com>
2020-04-08 09:11:11 -0600

    Pass test_returns_if_out_of_state

--- lib/contestant.rb
+++ lib/contestant.rb
@@ -6,7 +6,9 @@ class Contestant
     @age = attributes[:age]
     @state_of_residence = attributes[:state_of_residence]
     @spending_money = attributes[:spending_money]
+  end
 
-
+  def out_of_state?
+    @state_of_residence != 'CO'
   end
 end
