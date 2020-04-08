e25a9070540d9d1af59a377de26d818a7aad5af8
James Belta <james.belta@gmail.com>
2020-04-08 09:51:52 -0600

    Added Attributes test. Need to review booolean in game parameter)

--- lib/game.rb
+++ lib/game.rb
@@ -1,9 +1,10 @@
 class Game
 attr_reader :name, :cost, :national_drawing
 
-  def initialize (name, cost)
+  def initialize (name, cost, national_drawing)
     @name = name
     @cost = cost
     @national_drawing = national_drawing
+
   end
 end
--- test/game_test.rb
+++ test/game_test.rb
@@ -5,8 +5,18 @@ require './lib/game/'
 
 class GameTest < Minitest::Test
   def test_it_exist
-    pick_4 = Game.new('Pick 4', 2)
+    pick_4 = Game.new('Pick 4', 2, false)
 
     assert_instance_of Game, pick_4
+  end
+
+  def test_it_has_attributes
+    mega_millions = Game.new('Mega Millions', 5, true)
+    pick_4 = Game.new('Pick 4', 2, false)
+
+assert_equal 'Mega Millions', mega_millions.name
+assert_equal 5, mega_millions.cost
+assert_equal true, mega_millions.national_drawing
+assert_equal false, pick_4.national_drawing
   end
 end
