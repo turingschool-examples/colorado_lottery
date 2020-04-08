715e88590779155d604e8475fa9a2d06ffb690cc
Danny Ramos <danramos21@gmail.com>
2020-04-08 10:10:33 -0600

    Iteration 2

--- lib/colorado_lottery.rb
+++ lib/colorado_lottery.rb
@@ -1,11 +1,27 @@
 class ColoradoLottery
 
   attr_reader :registered_contestants,
-              :winners
+              :winners,
+              :current_contestants,
+              :name
 
   def initialize
     @registered_contestants = {}
     @winners = []
-    
+    @current_contestants = {}
+  end
+
+  def interested_and_18?(contestant, game_name)
+    if contestant.game_interests != game_name.name
+      return false
+    elsif contestant.game_interests == game_name.name && contestant.age >= 18
+      true
+    end
+  end
+
+  def can_register?(contestant, game_name)
+    if contestant.age >= 18 || contestant.game_interests == game_name.name
+      return true
+    end
   end
 end
--- test/colorado_lottery_test.rb
+++ test/colorado_lottery_test.rb
@@ -17,4 +17,86 @@ class ColoradoLotteryTest < Minitest::Test
     assert_equal ({}), lottery.current_contestants
   end
 
+  def test_contestant_interested_and_18
+    lottery = ColoradoLottery.new
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    winston = Contestant.new({
+                         first_name: 'Winston',
+                         last_name: 'Churchill',
+                         age: 18,
+                         state_of_residence: 'CO',
+                         spending_money: 5})
+    frederick = Contestant.new({
+                           first_name:  'Frederick',
+                           last_name: 'Douglas',
+                           age: 55,
+                           state_of_residence: 'NY',
+                           spending_money: 20})
+    benjamin = Contestant.new({
+                           first_name: 'Benjamin',
+                           last_name: 'Franklin',
+                           age: 17,
+                           state_of_residence: 'PA',
+                           spending_money: 100})
+    alexander = Contestant.new({
+                           first_name: 'Alexander',
+                           last_name: 'Aigades',
+                           age: 28,
+                           state_of_residence: 'CO',
+                           spending_money: 10})
+    alexander.add_game_interest('Pick 4')
+    alexander.add_game_interest('Mega Millions')
+    frederick.add_game_interest('Mega Millions')
+    winston.add_game_interest('Cash 5')
+    winston.add_game_interest('Mega Millions')
+    benjamin.add_game_interest('Mega Millions')
+    # assert_equal true, lottery.interested_and_18?(alexander, pick_4)
+    assert_equal false, lottery.interested_and_18?(benjamin, mega_millions)
+    assert_equal false, lottery.interested_and_18?(alexander, cash_5)
+  end
+
+  def test_contestant_can_register
+    lottery = ColoradoLottery.new
+    pick_4 = Game.new('Pick 4', 2)
+    mega_millions = Game.new('Mega Millions', 5, true)
+    cash_5 = Game.new('Cash 5', 1)
+    winston = Contestant.new({
+                         first_name: 'Winston',
+                         last_name: 'Churchill',
+                         age: 18,
+                         state_of_residence: 'CO',
+                         spending_money: 5})
+    frederick = Contestant.new({
+                           first_name:  'Frederick',
+                           last_name: 'Douglas',
+                           age: 55,
+                           state_of_residence: 'NY',
+                           spending_money: 20})
+    benjamin = Contestant.new({
+                           first_name: 'Benjamin',
+                           last_name: 'Franklin',
+                           age: 17,
+                           state_of_residence: 'PA',
+                           spending_money: 100})
+    alexander = Contestant.new({
+                           first_name: 'Alexander',
+                           last_name: 'Aigades',
+                           age: 28,
+                           state_of_residence: 'CO',
+                           spending_money: 10})
+    alexander.add_game_interest('Pick 4')
+    alexander.add_game_interest('Mega Millions')
+    frederick.add_game_interest('Mega Millions')
+    winston.add_game_interest('Cash 5')
+    winston.add_game_interest('Mega Millions')
+    benjamin.add_game_interest('Mega Millions')
+    assert_equal true, lottery.can_register?(alexander, pick_4)
+    # assert_equal false, lottery.can_register?(alexander, cash_5)
+    assert_equal true, lottery.can_register?(frederick, mega_millions)
+    # assert_equal false, lottery.can_register?(benjamin, mega_millions)
+    # assert_equal false, lottery.can_register?(frederick, cash_5)
+  end
+
 end
